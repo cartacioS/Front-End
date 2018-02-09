0f1d100e253852e43b1b1ace426e5246a8b0e966
cartacios <scart039@fiu.edu>
2018-02-09 10:43:55 -0500

    Added login page

---
+++ group-up/src/Routes.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Login from "./Containers/Login";
+export default () =>
+  <Switch>
+    <Route path="/" exact component={Login} />
+  </Switch>;
