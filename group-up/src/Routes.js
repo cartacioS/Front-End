937ee36fb58d188a365ad041d7d1a637f0c0cdff
Jake Lopez <jakedlopez@gmail.com>
2018-02-12 12:35:07 -0500

    configuration for eslint & prettier

--- group-up/src/Routes.js
+++ group-up/src/Routes.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./Containers/Login";
-export default () =>
+
+export default () => (
   <Switch>
     <Route path="/" exact component={Login} />
-  </Switch>;
+  </Switch>
+);
