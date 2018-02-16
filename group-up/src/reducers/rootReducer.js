54165a43a8e0722f25ac7d50cb94c39fbb3e9c56
Jake Lopez <jakedlopez@gmail.com>
2018-02-15 19:28:01 -0500

    more comments

--- group-up/src/reducers/rootReducer.js
+++ group-up/src/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 
+// the mother of all reducers. when you add a reducer make sure to add it to this function
 const rootReducer = combineReducers({ authReducer });
 
 export default rootReducer;
