448b80c52baf59ff8f568d355c929975699a0667
cartacios <scart039@fiu.edu>
2018-03-09 13:04:59 -0500

    Fixed login routing issue. Now default / and /login direct to login page

--- group-up/src/Routes.js
+++ group-up/src/Routes.js
@@ -7,6 +7,7 @@ import RequestForm from "./Containers/RequestForm"
 export default ({ location }) => (
   <Switch>
     <Route location={location} path="/" exact component={Login} />
+    <Route location={location} path="/Login" exact component={Login} />
     <Route location={location} path="/ListingsPage" exact component={ListingsPage} />
     <Route location={location} path="/register" exact component={Register} />
     <Route location={location} path="/requestform" exact component={RequestForm} />
