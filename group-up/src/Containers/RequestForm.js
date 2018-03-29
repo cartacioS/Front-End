07bb20a63fcd56bd8429de669109396439d09227
Sabina Cartacio <Sabina Cartacio>
2018-03-29 16:34:20 -0400

    Starting request form validation

--- group-up/src/Containers/RequestForm.js
+++ group-up/src/Containers/RequestForm.js
@@ -1,47 +1,83 @@
 import React, { Component } from "react";
-import { Button, FormGroup, ControlLabel, FormControl } from "react-bootstrap";
+import {FormGroup, ControlLabel, FormControl } from "react-bootstrap";
+import {isEmpty} from "lodash";
 import "../Components/RequestButton.css";
 import LoaderButton from "../Components/LoaderButton";
 import "./RequestForm.css";
-export default class Request extends Component {
+class Request extends React.Component {
    
-    constructor(props) {
-        super(props);
+    state = {
+        title: "",
+        expDate: "",
+        groupSize: "",
+        countNeeded: "",
+        description: "",
+        isLoading: false,
+        errors:{}
+    };
 
-        this.state = {
-            isLoading: false,
-            title: "",
-            expDate: "",
-            groupSize: "",
-            countNeeded: "",
-            description: ""
-        };
-    }
+    handleChange = e => {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    };    
 
-   validateForm() {
-        return this.state.title.length > 0;
+   validateData() {
+       const errors = {};
+       if(this.state.title === ""){
+           errors.title = "This field is required.";
+       }
+        return errors;
    }
 
-    render() {
-   return (
+    handleSubmit = e => {
+        e.preventDefault();
+        const errors = this.validateData();
+        const{
+            title,
+            expDate,
+            groupSize,
+            countNeeded,
+            description
+        } = this.state;
 
+        if(isEmpty(errors)){
+            this.setState({isLoading: false});
+        }else{
+            this.setState({errors, isLoading: false});
+        }
+    };
+
+
+    render() {
+        const {isLoading, errors} = this.state;
+
+   return (
      <div className="Request">
         <h1> Create Request </h1>
-        <form> 
-                <FormGroup controlId="title" bsSize="large">
+        <form onSubmit={this.handleSubmit}> 
+                <FormGroup
+                    controlId="title"
+                    bsSize="large"
+                    validationState = {errors.title ? "error" : null}
+                    >
+
                     <ControlLabel> Request Title </ControlLabel>
                         <FormControl
-                            controlId="title"
-                            autofocus
+                            autoFocus
                             placeholder="Enter Request Title"
-                            type="text"
+                            type="input"
+                            onChange={this.handleChange}
                         />
+                        {!!errors.title && (<ControlLabel>{errors.title} </ControlLabel>
+                        )}
+                    { console.log(this.state.title)}
                 </FormGroup>
                 <FormGroup controlId="expirationDate" bsSize="large">
                     <ControlLabel> Expiration Date </ControlLabel>
                         <FormControl
 
-                            autofocus
+                            autoFocus
                             placeholder="Enter Expiration Date"
                             type="text"
                         />
@@ -49,7 +85,7 @@ export default class Request extends Component {
                 <FormGroup controlId="groupSize" bsSize="large">
                     <ControlLabel> Group Size </ControlLabel>
                         <FormControl
-                            autofocus
+                            autoFocus
                             placeholder="Enter Group Size"
                             type="text"
                         />
@@ -57,7 +93,7 @@ export default class Request extends Component {
                 <FormGroup controlId="countNeeded" bsSize="large">
                     <ControlLabel> Count Needed </ControlLabel>
                         <FormControl
-                            autofocus
+                            autoFocus
                             placeholder="Enter Count Needed"
                             type="text"
                         />
@@ -65,7 +101,7 @@ export default class Request extends Component {
                 <FormGroup controlId="description" bsSize="large">
                     <ControlLabel> Description </ControlLabel>
                         <FormControl
-                            autofocus
+                            autoFocus
                             placeholder="Enter description"
                             type="text"
                         />
@@ -73,13 +109,12 @@ export default class Request extends Component {
                     <LoaderButton
                     block
                     bsSize="large"
-                    disabled={!this.validateForm()}
+                    disabled={!this.validateData()}
                     type="submit"
                     isLoading={this.state.isLoading}
                     text="Create"
                     loadingText="Creating Request..."
                     style={{ color:"white", backgroundColor:"#369B00"}}
-                    href="listings"
                  />
         </form>
 
@@ -87,3 +122,5 @@ export default class Request extends Component {
      );
    }
 }
+
+export default(Request);
